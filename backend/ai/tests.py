a86a22f9634a4da24fe2e8b2fd00eaa7d50e6ac1
brandon-kong <95515854+brandon-kong@users.noreply.github.com>
2024-04-07 01:56:46 -0500

    test

--- backend/ai/tests.py
+++ backend/ai/tests.py
@@ -1,13 +1,14 @@
 import os
+import pdb;
 from django.test import TestCase
 from ai.transcription import transcribe
 
 class TranscriptionTestCase(TestCase):
     def test_transcription(self):
         # Use an absolute path to avoid any confusion
-        audio_file_path = 'C:\\Users\\river\\Carpanion\\backend\\test_audio.mp3'
+        audio_file_path = './test_audio.mp3'
         print(f"Checking for file at: {audio_file_path}")
         self.assertTrue(os.path.isfile(audio_file_path), "Test audio file does not exist.")
-        import pdb; pdb.set_trace()
+        pdb.set_trace()
         transcription = transcribe(audio_file_path)
         self.assertIsNotNone(transcription)
